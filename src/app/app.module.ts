e2f7caf8b7b461f1d3d08158ce6a0f9c0b03812e
tuningfreelance <tuningfreelance@gmail.com>
2019-08-14 18:59:43 -0700

    added confirmation view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-//import {  } from '@angular/material/';
+import { FormsModule } from '@angular/forms';
+import { AppMaterialModule } from './app.material.module';
 
 import { AppComponent } from './app.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginFormComponent } from './views/login-form/login-form.component';
+import { ConfirmationComponent } from './views/confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
